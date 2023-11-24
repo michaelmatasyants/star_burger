fa648c9b0b59013a8942bd349f681ace8a104dea
Michael Matasyants <michael.matasyanc@gmail.com>
2023-11-24 23:40:23 +0300

    Initial commit

---
+++ foodcartapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'foodcartapp'
+
+urlpatterns = [
+    path('products/', views.product_list_api),
+    path('banners/', views.banners_list_api),
+    path('order/', views.register_order),
+]
